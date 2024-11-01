42f21f19b5ccabb0cd93eadc82fe5cacc326d303
RicardoRM <ricardorammtz@gmail.com>
2024-11-01 13:43:34 -0600

    dbUrl changed to MongoDB Atlas URL in app.js properly

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const mongoSanitize = require('express-mongo-sanitize')
 const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds')
 const reviewRoutes = require('./routes/reviews')
-// const dbUrl = process.env.DB_URL
-const dbUrl = 'mongodb://localhost:27017/yelp-camp'
+const dbUrl = process.env.DB_URL
+// const dbUrl = 'mongodb://localhost:27017/yelp-camp'
 
 mongoose.connect(dbUrl, {
 // mongoose.connect(dbUrl, {
